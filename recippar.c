30c899c8e84305e64fb99e2ea9d565cc500bc657
Alex Soong <asoong@ucdavis.edu>
2017-10-12 16:00:57 -0700

    trying to do in place

--- recippar.c
+++ recippar.c
@@ -1,75 +1,36 @@
 #include <omp.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <mpi.h>
 
 
 #define true 1
 #define false 0
 
-// tuple that holds data
-typedef struct {
-	int v1;
-	int v2;
-	int found_match;
-} tuple;
-
-
-// if match, add 1 to score
-int compare(tuple *t1, tuple *t2) {
-	if ((t1->v1 == t2->v1) && (t1->v2 == t2->v2)) {
-		// t1->found_match = true;
-		// t2->found_match = true;
-		return 1;
-	}
-
-	return 0;
-}
-
-// reverse tuple
-tuple* reverse(tuple *t) {
-	tuple *t2 = (tuple*) malloc(sizeof(tuple));
-	t2->v1 = t->v2;
-	t2->v2 = t->v1;
-
-	return t2;
-}
-
 // find num of reciprocal pairs
 int recippar(int **data, int N) {
 
 	int score = 0;
+	// int currentV1 = 0;
+	// int currentV2 = 0; 
+	// int tempV1 = 0;
+	// int tempV2 = 0; 
 
-	tuple* tuples = (tuple*) malloc(N * sizeof(tuple));
-	#pragma omp parallel 
-	{
-		int nth = omp_get_num_threads(); 
-		int me = omp_get_thread_num(); 
-
-		// populate array of tuples
-		#pragma omp for
-		for (int i = 0; i < N; i++) {
-			tuples[i].v1 = data[i][0];
-			tuples[i].v2 = data[i][1];
-			tuples[i].found_match = false;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			tuple *current = &tuples[i];// (tuple*) malloc(sizeof(tuple));
-			// current = &tuples[i];
-			if(current->found_match == false) {
-				current = reverse(&tuples[i]);
-				#pragma omp for
-				for (int j = 0; j < N; j++) {
-					if (compare(current, &tuples[j]) == 1) {
-						tuples[j].found_match = true;
-						tuples[i].found_match = true;
-						score += 1;
-					}
-				}
+	for (int i = 0; i < N; i++) {
+		// current vertex to be compared
+		int currentV1 = data[i][0];
+		int currentV2 = data[i][1]; 
+		for (int j = 0; j < 2; j++ ) {
+			int tempV1 = data[j][0];
+			int tempV2 = data[j][1];
+			if (currentV1 == tempV2 && currentV2 == tempV1) {
+				score += 1;
+				printf("match at: %d and %d\n", tempV1, tempV2);
 			}
-		}	
+		}
+
+		printf("i: %d", i);
 	}
-	
 	return score;
 }
 
@@ -82,8 +43,8 @@ int** generate_array(int N) {
 	}
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < 2; j++) {
-			arr[i][0] = rand() % 50;
-			arr[i][1] = rand() % 50;
+			arr[i][0] = rand() % 3;
+			arr[i][1] = rand() % 3;
 		}
 	}
 
@@ -93,8 +54,30 @@ int** generate_array(int N) {
 
 void main(int argc, char** argv) {
 
-	int N = 10000;
-	int** data_arr = generate_array(N);
+
+	int N = 10;
+	int** data_arr = (int**)malloc(10 * sizeof(int*));
+	for (int i = 0; i < N; i++) {
+		data_arr[i] = (int*)malloc(2 * sizeof(int));
+	}
+
+	// for (int i = 0; i < N; i++) {
+	// 	for (int j = 0; j < 2; j++) {
+	// 		data_arr[i][j] = rand() % 5 + 1;
+	// 	}
+	// }
+
+	data_arr[0][0] = 4; data_arr[0][1] = 2; 
+	data_arr[1][0] = 2; data_arr[1][1] = 1; 
+	data_arr[2][0] = 4; data_arr[2][1] = 5; 
+	data_arr[3][0] = 0; data_arr[3][1] = 6; 
+	data_arr[4][0] = 6; data_arr[4][1] = 0; 
+	data_arr[5][0] = 7; data_arr[5][1] = 9; 
+	data_arr[6][0] = 5; data_arr[6][1] = 8; 
+	data_arr[7][0] = 2; data_arr[7][1] = 8; 
+	data_arr[8][0] = 9; data_arr[8][1] = 7; 
+	data_arr[9][0] = 8; data_arr[9][1] = 5; 
+
 	for (int i = 0; i < N; i++) {
 		printf("a: %d ", data_arr[i][0]);
 		printf("b: %d\n", data_arr[i][1]);
