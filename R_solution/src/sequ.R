47bca5842e6b740bdcb8f3b39b7b674405aaf342
wmmxk <wmmkx@hotmail.com>
2017-10-17 00:05:49 -0700

    sequential solution done

--- R_solution/src/sequ.R
+++ R_solution/src/sequ.R
@@ -1,18 +1,12 @@
 source("bin_search.R")
 
-// test the binary function
-n= 7
-arr = seq(1,n,1)
-bin_search(arr,1,n,2)
-
 
 data =  read.table("../data/twitter_combined.txt", sep = " ")
 data = data[order(data[,1]),]
-col1= unlist(data[,1])
 
 data = data.matrix(data)
 
-
+col1= unlist(data[,1])
 
 score = 0 
 
@@ -20,22 +14,34 @@ N = nrow(data)
 
 for (i in 1:nrow(data)) {
 		row = unlist(data[i,])
-		
-
 		index = bin_search(col1,1,N,row[2])
 		
-		# indexes = seq(1,100,1)
-
-    if (i%%20000==0) {cat(i,"\n")}
+  if (i%%20000==0) {cat(i,"\n")}
 		
-		# if (length(indexes) > 0) {
-		#   for (index in indexes) {
-		#     if (data[2,index] == row[1] ) {
-		#     score = score +1
-		#     break
-		#     }
-		#   }
-		# }
+		if (index!=-1) { 
+						found = FALSE
+						i = index
+						while (col1[i] == row[2] && i > 0) {
+									if (data[i,2] == row[1]) {
+												score = score +1
+												found = TRUE
+												break
+									}
+									i = i-1
+						}
+						
+						if (!found) {
+									i=index+1
+									while (col1[i] == row[2] && i <=N) {
+											if (data[i,2] == row[1]) {
+														score = score +1
+														break
+													}
+											i = i+1
+									}
+						}
+		 }
 }
 
+score = score/2 -7 // 7 is the number of reflexive nodes
 
