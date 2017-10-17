fe8bf9fc35c8e05512e2af70707230c7906e9061
wmmxk <wmmkx@hotmail.com>
2017-10-16 23:32:58 -0700

    add a binary search

--- R_solution/src/sequ.R
+++ R_solution/src/sequ.R
@@ -1,30 +1,41 @@
+source("bin_search.R")
+
+// test the binary function
+n= 7
+arr = seq(1,n,1)
+bin_search(arr,1,n,2)
+
+
 data =  read.table("../data/twitter_combined.txt", sep = " ")
 data = data[order(data[,1]),]
+col1= unlist(data[,1])
 
-data= t(data.matrix(data))
-data_rev = data[,c(2,1)]
+data = data.matrix(data)
+
+
 
 score = 0 
 
+N = nrow(data)
 
-for (i in 1:ncol(data)) {
-		row = unlist(data[,i])
+for (i in 1:nrow(data)) {
+		row = unlist(data[i,])
 		
 
-		# indexes = which(data[,1] == row[2])
+		index = bin_search(col1,1,N,row[2])
 		
-		indexes = seq(1,100,1)
+		# indexes = seq(1,100,1)
 
     if (i%%20000==0) {cat(i,"\n")}
 		
-		if (length(indexes) > 0) {
-		  for (index in indexes) {
-		    if (data[2,index] == row[1] ) {
-		    score = score +1
-		    break
-		    }
-		  }
-		}
+		# if (length(indexes) > 0) {
+		#   for (index in indexes) {
+		#     if (data[2,index] == row[1] ) {
+		#     score = score +1
+		#     break
+		#     }
+		#   }
+		# }
 }
 
 
